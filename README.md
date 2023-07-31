d40156d95e538e2f7daf599e17f544dac81f0d6d
Floride125-art <flo.tuyisenge@gmail.com>
2023-08-01 00:40:47 +0530

    working on my project

---
+++ README.md
@@ -0,0 +1,6 @@
+## Etch-A-Sketch
+This project is for creating a pretty neat toy for your portfolio to flex your DOM manipulation skills.
+## Technologies used
+HTML, CSS and Javscript
+## installation and running
+clone the project by git clone  in your local machine and run it or copy deployed live link of project and then run it in your browser.
---
+++ script.js
@@ -0,0 +1,59 @@
+const container = document.createElement('div');
+container.id = 'container';
+container.className = 'mx-auto my-2';
+
+document.body.appendChild(container);
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  container.style.border = '3px solid #FFFFFF'
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("mouseover", paint);
+  };
+};
+
+const color = document.getElementById('favcolor');
+
+function paint(e) {
+  let chooseColor = color.value;
+  e.target.style.backgroundColor= chooseColor;
+}
+
+let size = 0;
+const start = document.getElementById('start');
+start.addEventListener('click', function(){
+  size = prompt('Please enter the side of the grid(max 64):');
+    if(size <= 64){
+      container.innerHTML = '';
+      makeRows(size, size);
+    } else if (size > 64) {
+      alert('You exceeded the limits. Please Try Again:');
+    } 
+});
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', function(){
+  container.innerHTML = '';
+  makeRows(size, size);
+});
+
+document.addEventListener("keypress", function stopPaint(event){
+  let x = event.keyCode;
+  const grid = document.getElementsByClassName('grid-item');
+  if(x !== 13) {
+    Array.from(grid).forEach((el) => {
+      el.removeEventListener("mouseover", paint);  
+    });
+  } else {
+    Array.from(grid).forEach((el) => {
+      el.addEventListener("mouseover", paint);
+    })
+  };
+});
+
+
+
+
